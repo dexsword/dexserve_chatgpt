2271ec3e6c534bbc4f2eeaacdb9799b1239cd93a
Will <wlogatto@gmail.com>
2023-03-20 22:22:19 -0700

    First GPT Updates

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.querySelector('.tip-button').addEventListener('click', () => {
+	window.open('https://ftx.us/pay/request?coin=SOL&address=CW2zUdgE8nrY67bWoTdE9iS5uRX95dK8nyWU9bSyUNuU&tag=&wallet=sol&memoIsRequired=false&memo=&allowTip=true', '_blank', 'resizable,width=700,height=900');
+});
